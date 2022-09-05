29ac000d79d1ed31cfbb872b1d0660b17a804e4b
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-09-04 22:03:14 -0400

    fastify init

--- flake.nix
+++ flake.nix
@@ -10,6 +10,6 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in with pkgs; {
-        devShell = pkgs.mkShell { buildInputs = [ nodejs-16_x ]; };
+        devShell = pkgs.mkShell { buildInputs = [ nodejs-18_x node2nix ]; };
       });
 }
