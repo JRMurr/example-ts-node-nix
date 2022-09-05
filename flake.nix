ca2bb3249af4a8962ef156114d5fda2c25e897b0
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-09-04 21:35:32 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Sample Nix ts-node build";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; {
+        devShell = pkgs.mkShell { buildInputs = [ nodejs-16_x ]; };
+      });
+}
