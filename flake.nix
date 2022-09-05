55f16d4542d03cd65d294f9473b0141de638ff2b
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-09-05 15:48:00 -0400

    attempt build

--- flake.nix
+++ flake.nix
@@ -4,12 +4,48 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, gitignore, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        nodejs = pkgs.nodejs-18_x;
+
+        appBuild = pkgs.stdenv.mkDerivation {
+          name = "example-ts-node";
+          version = "0.1.0"; # TODO: parse from package.json
+          src = gitignore.lib.gitignoreSource ./.;
+          buildInputs = [ nodejs ];
+          # https://nixos.org/manual/nixpkgs/stable/#sec-stdenv-phases
+          buildPhase = ''
+            runHook preBuild
+            npm ci # sad does not work, no internet access in pure mode
+            npm run build
+            runHook postBuild
+          '';
+          # checkPhase = ''
+          #   runHook preCheck
+          #   npm run test
+          #   runHook postCheck
+          # '';
+          installPhase = ''
+            runHook preInstall
+
+            cp -r node_modules $out/node_modules
+            cp package.json $out/package.json
+            cp -r dist $out/dist
+
+            runHook postInstall
+          '';
+        };
+
       in with pkgs; {
-        devShell = pkgs.mkShell { buildInputs = [ nodejs-18_x node2nix ]; };
+        defaultPackage = appBuild;
+        devShell = mkShell { buildInputs = [ nodejs ]; };
       });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import fastify from "fastify";
 
 const server = fastify();
