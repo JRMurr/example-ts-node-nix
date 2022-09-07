e1996346c5d6575e2bd2e5f7974c2d56fb9514c6
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-09-06 21:18:16 -0400

    use dream2nix

---
+++ README.md
@@ -0,0 +1,3 @@
+# Example Typescript Node App
+
+Simple fastify node app in typescript with nix builders. 
--- flake.nix
+++ flake.nix
@@ -8,44 +8,13 @@
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    dream2nix.url = "github:nix-community/dream2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, gitignore, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        nodejs = pkgs.nodejs-16_x;
-
-        appBuild = pkgs.stdenv.mkDerivation {
-          name = "example-ts-node";
-          version = "0.1.0"; # TODO: parse from package.json
-          src = gitignore.lib.gitignoreSource ./.;
-          buildInputs = [ nodejs ];
-          # https://nixos.org/manual/nixpkgs/stable/#sec-stdenv-phases
-          buildPhase = ''
-            runHook preBuild
-            npm ci # sad does not work, no internet access in pure mode
-            npm run build
-            runHook postBuild
-          '';
-          # checkPhase = ''
-          #   runHook preCheck
-          #   npm run test
-          #   runHook postCheck
-          # '';
-          installPhase = ''
-            runHook preInstall
-
-            cp -r node_modules $out/node_modules
-            cp package.json $out/package.json
-            cp -r dist $out/dist
-
-            runHook postInstall
-          '';
-        };
-
-      in with pkgs; {
-        defaultPackage = appBuild;
-        devShell = mkShell { buildInputs = [ nodejs ]; };
-      });
+  outputs = { self, nixpkgs, flake-utils, gitignore, dream2nix, ... }:
+    dream2nix.lib.makeFlakeOutputs {
+      systems = flake-utils.lib.defaultSystems;
+      config.projectRoot = ./.;
+      source = gitignore.lib.gitignoreSource ./.;
+    };
 }
